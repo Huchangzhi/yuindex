fb6a1271a07747c8e204fe520b97a6a03f5af471
编程小白 <138838812+Huchangzhi@users.noreply.github.com>
2024-10-27 16:06:02 +0800

    bug

--- src/core/commands/ddos/DdosBox.vue
+++ src/core/commands/ddos/DdosBox.vue
@@ -8,16 +8,11 @@
       </a>
 
     </div>
-    <img
-      width="200"
-   
-      src="http://111.229.80.36:5244/d/123/hehe.gif?sign=--iRI2d2padEEq6gbf8b-hMaKT1zv2cS43Fr3H3AazU=:0"
-      alt="做个好人"
-    />
+    
     <img
       width="200"
       style="margin: 8px 0"
-      src="https://s1.ax1x.com/2022/07/19/j76SQU.png"
+      src="http://111.229.80.36:5244/d/123/hehe.gif?sign=--iRI2d2padEEq6gbf8b-hMaKT1zv2cS43Fr3H3AazU=:0"
       alt="做个好人"
     />
   </div>
